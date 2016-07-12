3258737279d850e6144d26567cb5a244e8241f72
Paul <pacalabre@gmail.com>
2016-07-12 12:32:08 -0700

    added basic routes to home, dashboard, and job deck pages

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 var bodyParser = require('body-parser');
 var path = require('path');
 
 app.set('port', 3000);
 
+app.use(bodyParser.urlencoded({ extended : false }));
 
+app.get('/', function(req, res) {
+  console.log("got the homepage");
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/dashboard', function(req, res) {
+  console.log("got the dashboard");
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+})
+
+app.get('/dashboard/:id', function(req, res) {
+  console.log("got the job page");
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'job.html'));
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(bodyParser.urlencoded({ extended : false }));
 
 
 var server = app.listen(app.get('port'), function() {
