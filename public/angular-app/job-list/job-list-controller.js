1345f62d48679279a9a1c9540f38997960cae4cf
Paul <pacalabre@gmail.com>
2016-08-09 13:41:30 -0700

    bootstrap working again

--- public/angular-app/job-list/job-list-controller.js
+++ public/angular-app/job-list/job-list-controller.js
@@ -3,9 +3,12 @@ angular.module('JobsApp').controller('JobsController', JobsController);
 function JobsController(jobDataFactory) {
   var vm = this;
   vm.myname = "Paul";
+
   vm.title = 'MEAN Hotel App';
   jobDataFactory.jobList().then(function(response){
     console.log(response.data);
     vm.jobs = response.data;
   })
 }
+
+
